de51a1e54c278620483c6ddff5e1cf123d5fe47e
jovisf <jovisf30@gmail.com>
2026-02-10 08:44:38 -0300

    feat(dashboard): add useLocations hook and SearchableSelect component

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,6 +3,6 @@ export { useRequireAuth, useRedirectIfAuthenticated } from './useProtectedRoute'
 export { useDebounce } from './useDebounce';
 export * from './tickets';
 export { useSimulator } from './simulator/useSimulator';
-export { useDashboard } from './dashboard';
+export * from './dashboard';
 export { useChat } from './chat';
 export { useHeader } from './useHeader';
