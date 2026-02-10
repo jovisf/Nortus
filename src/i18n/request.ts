074fbc7a171b9437ac13c03fa9b92ac08db4c126
jovisf <jovisf30@gmail.com>
2026-02-10 13:38:06 -0300

    feat(i18n): setup next-intl and add translation files

---
+++ src/i18n/request.ts
@@ -0,0 +1,13 @@
+import { getRequestConfig } from 'next-intl/server';
+import { cookies } from 'next/headers';
+
+export default getRequestConfig(async () => {
+    // We can get the locale from a cookie or session if not using [locale] routing
+    const cookieStore = await cookies();
+    const locale = cookieStore.get('NEXT_LOCALE')?.value || 'pt-BR';
+
+    return {
+        locale,
+        messages: (await import(`../../messages/${locale}.json`)).default
+    };
+});
